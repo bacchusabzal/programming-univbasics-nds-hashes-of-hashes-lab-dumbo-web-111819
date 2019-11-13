aee486964394c132405d807bd8cedc572223e47c
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-13 06:03:28 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,7 +43,7 @@ def naming_system
   # HoH all the way to Species!
   
 biological_life_on_earth = PORTION_1
-PORTION_1[:sub_category][:sub_category] = PORTION_2
+PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
 PORTION_2[:sub_category] = PORTION_3
 PORTION_3[:sub_category][:sub_category] = PORTION_4
 
